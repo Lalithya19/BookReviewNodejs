15dc3ea4f727ef0e415f05b1ddbdf28875e632a8
Sathukumati, Lalithya <lalithya.sathukumati@accenture.com>
2026-02-25 15:15:15 +0530

    Updated general.js file

--- general.js
+++ general.js
@@ -1,56 +1,57 @@
-const axios = require("axios");
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+const axios = require('axios'); // Ensure axios is required
 
-// Task 10: Get the book list available in the shop using Promises
-public_users.get("/", function (req, res) {
-  const getBooks = new Promise((resolve, reject) => {
-    resolve(books);
-  });
-  getBooks.then((bookList) => {
-    res.status(200).send(JSON.stringify(bookList, null, 4));
-  });
-});
-
-// Task 11 (ISBN) - Using Promises
-public_users.get("/isbn/:isbn", function (req, res) {
-  const isbn = req.params.isbn;
-  const getBook = new Promise((resolve, reject) => {
-    if (books[isbn]) {
-      resolve(books[isbn]);
-    } else {
-      reject({ status: 404, message: "Book not found" });
-    }
-  });
-  getBook
-    .then((book) => res.status(200).json(book))
-    .catch((err) => res.status(err.status).json({ message: err.message }));
-});
-
-// Task 12: Get book details based on author using Async/Await
-public_users.get("/author/:author", async function (req, res) {
-  const author = req.params.author;
-  try {
-    const getByAuthor = await new Promise((resolve) => {
-      let filteredBooks = Object.values(books).filter(
-        (b) => b.author === author,
-      );
-      resolve(filteredBooks);
+// Task 10: Get the list of books available in the shop using Promises
+public_users.get('/', function (req, res) {
+    const getBooks = new Promise((resolve, reject) => {
+        resolve(books);
     });
-    res.status(200).send(JSON.stringify(getByAuthor, null, 4));
-  } catch (error) {
-    res.status(500).json({ message: "Error retrieving books" });
-  }
+
+    getBooks.then((bookList) => {
+        res.status(200).send(JSON.stringify(bookList, null, 4));
+    });
 });
 
-// Task 13: Get book details based on title using Async/Await
-public_users.get("/title/:title", async function (req, res) {
-  const title = req.params.title;
-  try {
-    const getByTitle = await new Promise((resolve) => {
-      let filteredBooks = Object.values(books).filter((b) => b.title === title);
-      resolve(filteredBooks);
+// Task 11: Get book details based on ISBN using Promises
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const getBookByISBN = new Promise((resolve, reject) => {
+        if (books[isbn]) {
+            resolve(books[isbn]);
+        } else {
+            reject({ message: "Book not found" });
+        }
     });
-    res.status(200).send(JSON.stringify(getByTitle, null, 4));
-  } catch (error) {
-    res.status(500).json({ message: "Error retrieving books" });
-  }
+
+    getBookByISBN
+        .then((book) => res.status(200).send(JSON.stringify(book, null, 4)))
+        .catch((err) => res.status(404).send(err.message));
 });
+
+// Task 12: Get book details based on Author using Promises
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    const getBooksByAuthor = new Promise((resolve, reject) => {
+        const filteredBooks = Object.values(books).filter(b => b.author === author);
+        resolve(filteredBooks);
+    });
+
+    getBooksByAuthor.then((filtered) => res.status(200).send(JSON.stringify(filtered, null, 4)));
+});
+
+// Task 13: Get book details based on Title using Promises
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title;
+    const getBooksByTitle = new Promise((resolve, reject) => {
+        const filteredBooks = Object.values(books).filter(b => b.title === title);
+        resolve(filteredBooks);
+    });
+
+    getBooksByTitle.then((filtered) => res.status(200).send(JSON.stringify(filtered, null, 4)));
+});
+
+module.exports.general = public_users;
